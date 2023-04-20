7822186d132d5f76680447b98082935c5aff387a
OVIDIU121 <ovidiu_121@yahoo.com>
2023-04-20 13:43:26 +0100

    Added sign-up form

--- WMS/webApplication/urls.py
+++ WMS/webApplication/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name = "login"),
+    path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
+    path("pricing", views.pricing_view, name="pricing"),
+    path('signup/', views.signup_view, name='signup'),
 
 ]
--- WMS/webApplication/views.py
+++ WMS/webApplication/views.py
@@ -1,22 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth.models import User
+from .forms import SignUpForm
 
 # Create your views here.
+
 
 def index(request):
     if not request.user.is_authenticated:
         return render(request, "webApplication/index.html")
     return render(request, "webApplication/user.html")
 
+
 def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
-        if user  is not None:
+        if user is not None:
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
@@ -25,8 +28,33 @@ def login_view(request):
             })
     return render(request, "webApplication/login.html")
 
+
 def logout_view(request):
     logout(request)
     return render(request, "webApplication/login.html", {
         "message": "Logged out."
     })
+
+
+def pricing_view(request):
+    return render(request, "webApplication/pricing.html")
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = request.POST["username"]
+            raw_password = request.POST["password1"]
+            confirm_password = request.POST["password2"]
+            raw_email = request.POST["email"]
+            raw_fn = request.POST["first_name"]
+            raw_ln = raw_fn = request.POST["first_name"]
+
+            user = authenticate(username=username, password=raw_password, )
+            login(request, user)
+            return redirect("webApplication/user.html")
+    else:
+        form = SignUpForm()
+    return render(request, 'webApplication/signup.html', {'form': form})
