8b6de63c7a6cdd186ff82809c289bca4bf9f8b17
OVIDIU121 <ovidiu_121@yahoo.com>
2023-04-22 00:09:58 +0100

    Switched to AWS PostGres database.

--- WMS/WMS/settings.py
+++ WMS/WMS/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'WMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'ovidiu121',
+        'PASSWORD': 'Sempron3000!',
+        'HOST': 'wms-django-db.c97hka3r8i0n.eu-west-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
