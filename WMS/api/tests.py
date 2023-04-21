fde1f39bdcb854c77d8bbe19d6cc3d41706e05c0
OVIDIU121 <ovidiu_121@yahoo.com>
2023-04-21 23:18:27 +0100

    Added tests for models and views.

--- WMS/api/tests.py
+++ WMS/api/tests.py
@@ -1,3 +1,68 @@
 from django.test import TestCase
+from .models import Customer, Supplier, Item
 
-# Create your tests here.
+
+class CustomerModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Customer.objects.create(
+            name='John Smith', address='123 Main St', code='ABC123')
+
+    def test_name_label(self):
+        customer = Customer.objects.get(id=1)
+        field_label = customer._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
+
+    def test_code_max_length(self):
+        customer = Customer.objects.get(id=1)
+        max_length = customer._meta.get_field('code').max_length
+        self.assertEqual(max_length, 255)
+
+    def test_str_representation(self):
+        customer = Customer.objects.get(id=1)
+        expected_str = f'Name: {customer.name}, Address: {customer.address}, Code: {customer.code}'
+        self.assertEqual(str(customer), expected_str)
+
+
+class SupplierModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Supplier.objects.create(
+            name='ABC Corp', address='456 Main St', code='XYZ789')
+
+    def test_name_label(self):
+        supplier = Supplier.objects.get(id=1)
+        field_label = supplier._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
+
+    def test_code_max_length(self):
+        supplier = Supplier.objects.get(id=1)
+        max_length = supplier._meta.get_field('code').max_length
+        self.assertEqual(max_length, 255)
+
+    def test_str_representation(self):
+        supplier = Supplier.objects.get(id=1)
+        expected_str = f'Name: {supplier.name}, Address: {supplier.address}, Code: {supplier.code}'
+        self.assertEqual(str(supplier), expected_str)
+
+
+class ItemModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        supplier = Supplier.objects.create(
+            name='ABC Corp', address='456 Main St', code='XYZ789')
+        Item.objects.create(name='Widget', sku='WID001', description='A useful widget',
+                            weight=1.5, height=2.0, width=2.0, depth=1.0, supplier=supplier)
+
+    def test_name_label(self):
+        item = Item.objects.get(id=1)
+        field_label = item._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
+
+    def test_str_representation(self):
+        item = Item.objects.get(id=1)
+        expected_str = f'Name: {item.name}, Sku: {item.sku}, Description: {item.description}'
+        self.assertEqual(str(item), expected_str)
--- WMS/webApplication/tests.py
+++ WMS/webApplication/tests.py
@@ -1,3 +1,64 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .forms import SignUpForm
 # Create your tests here.
+
+
+class LoginViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse("login")
+
+    def test_login_view_with_invalid_credentials(self):
+        username = "testuser"
+        password = "testpassword"
+        User.objects.create_user(username=username, password=password)
+        response = self.client.post(
+            self.login_url, {"username": username, "password": "wrongpassword"})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "webApplication/login.html")
+        self.assertContains(response, "Invalid credentials")
+
+
+class LogoutViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.logout_url = reverse("logout")
+
+    def test_logout_view(self):
+        response = self.client.get(self.logout_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "webApplication/login.html")
+        self.assertContains(response, "Logged out")
+
+
+class PricingViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.pricing_url = reverse("pricing")
+
+    def test_pricing_view(self):
+        response = self.client.get(self.pricing_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "webApplication/pricing.html")
+
+
+class SignUpViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('signup')
+        self.valid_data = {
+            'username': 'testuser',
+            'email': 'testuser@test.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'password1': 'testpassword',
+            'password2': 'testpassword',
+        }
+
+    def test_get_request(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'webApplication/signup.html')
+        self.assertIsInstance(response.context['form'], SignUpForm)
